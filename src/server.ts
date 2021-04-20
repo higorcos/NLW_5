c539d1c4e9086ecfc23523ced445d1acfd0adfd2
higor costa <higorcosta2001@outlook.com>
2021-04-20 20:47:49 -0300

    Port

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,5 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, ()=> console.log("__________Server is running on port 3000"));
+const port = 3000;
+app.listen(port, ()=> console.log(`__________Server is running on port ${port}`));
